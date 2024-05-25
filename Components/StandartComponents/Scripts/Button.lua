724b83761d9cdf35a2942d9f6bc8360848d9f585
Zabergeier <168218004+Zabergeier@users.noreply.github.com>
2024-05-25 22:10:37 +0200

    Update Button.lua

--- Components/StandartComponents/Scripts/Button.lua
+++ Components/StandartComponents/Scripts/Button.lua
@@ -1,1 +1,26 @@
+local Button = {}
 
+Button.__index = Button
+
+Button.__call = function(v1)
+	return v1.btn
+end
+
+
+function Button.new(_name:string,_parent:Instance)
+	local self = setmetatable(Button,{})
+	--Def Ui
+	
+	local btn = Instance.new("ImageButton",_parent)
+	btn.Name = _name
+	btn.Size = UDim2.new(0,100,0,100)
+	btn.Position = UDim2.new(0.5,0,0.5,0)
+	self.btn = btn
+	
+	
+	
+	
+	return self
+end
+
+return Button
